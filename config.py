e93819343a47f745309f65200dc0c8495ba1b47c
Bsecurity <147877003+Bsecurity@users.noreply.github.com>
2023-10-19 04:40:13 +0800

    Create config.py
    A configuration file containing placeholders for ClickSend API credentials and the recipient's phone number. Users should update with their actual details before using the script.

---
+++ config.py
@@ -0,0 +1,9 @@
+# config.py
+
+# Your ClickSend API credentials
+CLICKSEND_USERNAME = 'CLICK SEND USERNAME HERE'
+CLICKSEND_PASSWORD = 'CLICK SEND API KEY HERE'
+
+RECIPIENT_PHONE_NUMBER = '+61 MOBILE NUMBER HERE'
+
+# Any other configuration constants can go here.
